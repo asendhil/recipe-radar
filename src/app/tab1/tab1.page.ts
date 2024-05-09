100948007cee1355d36197cf4d0409f693ca578a
Sanika Bhamare <sanikabhamare@gmail.com>
2024-05-09 11:43:43 -0700

    adding swiping functionality to cards

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,5 +8,42 @@ import { Component } from '@angular/core';
 export class Tab1Page {
 
   constructor() {}
+  currentRecipeIndex: number = 0; // Start at the first recipe
+  recipes = [
+    {
+      name: 'Spaghetti Bolognese',
+      subtitle: 'Time taken: 30min',
+      img: 'https://ionicframework.com/docs/img/demos/card-media.png',
+      description: 'A classic Italian pasta dish with a rich tomato sauce.',
+    },
+    {
+      name: 'Grilled Chicken',
+      subtitle: 'Time taken: 25min',
+      img: 'https://ionicframework.com/docs/img/demos/card-media.png',
+      description: 'Juicy grilled chicken with herbs and spices.',
+    },
+    {
+      name: 'Vegetable Stir Fry',
+      subtitle: 'Time taken: 20min',
+      img: 'https://ionicframework.com/docs/img/demos/card-media.png',
+      description: 'A healthy mix of fresh vegetables.',
+    },
+  ];
 
+  // Move to the next recipe in the list
+  nextRecipe() {
+    this.currentRecipeIndex = (this.currentRecipeIndex + 1) % this.recipes.length;
+  }
+
+  // Bookmark the current recipe and move to the next one
+  bookmarkRecipe() {
+    // Here, you'd typically add a bookmark action, such as saving to a database or local storage
+    console.log('Bookmarked:', this.recipes[this.currentRecipeIndex].name);
+    this.nextRecipe();
+  }
+
+  // Skip the current recipe and move to the next one
+  skipRecipe() {
+    this.nextRecipe();
+  }
 }
