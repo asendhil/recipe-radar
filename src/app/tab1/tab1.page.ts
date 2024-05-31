d339cdf839c5ddbad88588969412f11a7d147fed
Sanika Bhamare <sanikabhamare@gmail.com>
2024-05-30 22:14:57 -0700

    reverted to previous commit and added data to assests folder

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,44 +1,26 @@
 import { Component } from '@angular/core';
 import { RecipeService } from '../recipe.service';
-
+import { recipes } from 'src/assets/data/recipes';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  recipes = recipes;
   saved: { name: string; subtitle: string; img: string; description: string; }[] = [];
   constructor(private recipeService: RecipeService) {
   }
   currentRecipeIndex: number = 0; // Start at the first recipe
-  recipes = [
-    {
-      name: 'Spaghetti Bolognese',
-      subtitle: 'Time taken: 30min',
-      img: 'https://ionicframework.com/docs/img/demos/card-media.png',
-      description: 'A classic Italian pasta dish with a rich tomato sauce.',
-    },
-    {
-      name: 'Grilled Chicken',
-      subtitle: 'Time taken: 25min',
-      img: 'https://ionicframework.com/docs/img/demos/card-media.png',
-      description: 'Juicy grilled chicken with herbs and spices.',
-    },
-    {
-      name: 'Vegetable Stir Fry',
-      subtitle: 'Time taken: 20min',
-      img: 'https://ionicframework.com/docs/img/demos/card-media.png',
-      description: 'A healthy mix of fresh vegetables.',
-    },
-  ];
+  
 
   // Move to the next recipe in the list
   nextRecipe() {
-    this.currentRecipeIndex = (this.currentRecipeIndex + 1) % this.recipes.length;
+    this.currentRecipeIndex = (this.currentRecipeIndex + 1) % recipes.length;
   }
 
   bookmarkRecipe() {
-    const currentRecipe = this.recipes[this.currentRecipeIndex];
+    const currentRecipe = recipes[this.currentRecipeIndex];
     const isAlreadySaved = this.recipeService.savedRecipes.some(recipe => recipe.name === currentRecipe.name);
     if (!isAlreadySaved) {
       this.recipeService.savedRecipes.push(currentRecipe);
---
+++ src/assets/data/recipes.ts
@@ -0,0 +1,20 @@
+export const recipes = [
+    {
+      name: 'Spaghetti Bolognese',
+      subtitle: 'Time taken: 30min',
+      img: 'https://ionicframework.com/docs/img/demos/card-media.png',
+      description: 'A classic Italian pasta dish with a rich tomato sauce.',
+    },
+    {
+      name: 'Grilled Chicken',
+      subtitle: 'Time taken: 25min',
+      img: 'https://ionicframework.com/docs/img/demos/card-media.png',
+      description: 'Juicy grilled chicken with herbs and spices.',
+    },
+    {
+      name: 'Vegetable Stir Fry',
+      subtitle: 'Time taken: 20min',
+      img: 'https://ionicframework.com/docs/img/demos/card-media.png',
+      description: 'A healthy mix of fresh vegetables.',
+    },
+  ];
