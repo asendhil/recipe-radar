7856791d0e85516bf2f00c5e829ea6547840b50c
unknown <aasha.sendhil@gmail.com>
2024-06-05 16:31:19 -0700

    adding recipes

--- src/assets/data/recipes.ts
+++ src/assets/data/recipes.ts
@@ -26,6 +26,106 @@ export const recipes = [
       nutrition: [],
       directions:[]
     },
+    {
+      name: 'Denver Omelet Salad',
+      subtitle: 'Time taken: 25min',
+      img: 'https://ionicframework.com/docs/img/demos/card-media.png',
+      description: 'A scrumptious breakfast meal made with avocados, cherry tomatoes and fried eggs. A perfect blend of protein, fat and essential macronutrients to keep you going for the day!',
+      ingredients: ['Fresh baby spinach (8 cups)', 'Chopped tomatoes (1 cup)', 'Olive oil (2 tbsp)', 'Chopped cooked ham (1.5 cup)', 'Small onion chopped (1)', 'Small green pepper chopped (1)',
+      'Large eggs (4)', 'Salt and pepper to taste'],
+      nutrition: [],
+      directions:['Arrange spinach and tomatoes on a platter; set aside. In a large skillet, heat 1 tablespoon olive oil over medium-high heat. Add ham, onion and green pepper; saute until ham is heated through and vegetables are tender, 5-7 minutes. Spoon over spinach and tomatoes.',
+      'In the same skillet, heat remaining olive oil over medium heat. Break eggs, 1 at a time, into a small cup, then gently slide into the skillet. Immediately reduce heat to low; season with salt and pepper. To prepare sunny-side up eggs, cover pan and cook until whites are completely set and yolks thicken but are not hard. Top salad with fried eggs.']
+    },
+    {
+      name: 'Breakfast Quesadillas',
+      subtitle: 'Time taken: 25min',
+      img: 'https://ionicframework.com/docs/img/demos/card-media.png',
+      description: 'These Easy Breakfast Quesadillas are filled with fluffy scrambled eggs, green peppers, bacon and cheddar cheeses, all enveloped between two crispy tortilla shells. It’s an easy meal that’s perfect for busy mornings!',
+      ingredients: ['Eggs (2)', 'Pinto/black beans (1/3 cup)', 'Butter (2 tsp)', 'Chopped green onion (1)', 'Finely chopped cilantro (1 tbsp)', 'Chopped jalepeno (1 tbsp)', 'Whole grain tortilla (1)', 'Grated sharp cheddar (1/3 cup)'],
+      nutrition: [],
+      directions: ['To prepare the eggs: In a bowl, whisk the eggs with the hot sauce and salt until they are well blended. Add the beans and set aside.',
+      'To cook the eggs: Melt the butter in a medium-sized skillet (either well-seasoned cast iron or nonstick) over medium heat until it’s bubbling. Pour in the egg mixture and cook, stirring often, until the eggs are just set, about 1 to 3 minutes. Transfer the mixture to a bowl to pause the cooking process (the eggs will finish cooking in the quesadilla). Stir in the green onion, cilantro, and jalapeño.',
+      'To cook the quesadilla: In a separate, large skillet, warm the tortilla over medium heat, flipping occasionally. Once the pan and tortilla are warm, sprinkle one-half of the cheese over one-half of the tortilla. Top the cheese with scrambled eggs, then top the scrambled eggs with the remaining cheese.',
+      'Press the empty tortilla halve over the toppings. Let the quesadilla cook until golden and crispy on the bottom (don’t stop cooking too soon!), about 1 to 2 minutes, reducing the heat if necessary to prevent burning the tortilla. Flip it and cook until the second side is golden and crispy.',
+      'Immediately remove the skillet from the heat and transfer the quesadilla to a cutting board. Let it cool for a few minutes to give it time to set, then slice each quesadilla into 2 slices with a very sharp knife. Serve immediately, with your favorite salsa and/or hot sauce on the side.']
+    },
+    {
+      name: 'Sweet and Spicy Turkey Sandwich',
+      subtitle: 'Time taken: 15min',
+      img: 'https://ionicframework.com/docs/img/demos/card-media.png',
+      description: 'Pepperjack cheese and strawberry preserves mingle on this turkey sandwich for a sweet and savory lunch. This sandwich has a good combination of sweet and spicy flavor.',
+      ingredients: ['Hearty country break (2 slices)', 'Roasted turkey breast (4 slices)', 'Pepperjack cheese (1 slice)', 'Butter (2 tsp)', 'Strawberry preserves (4 tsp)'],
+      nutrition: [],
+      directions: ['Combine crumbled chorizo and chipotle peppers in adobo sauce in a bowl.',
+      'Heat a skillet over medium-high heat; add chorizo mixture and cook until crisp, 5 to 7 minutes. Transfer to a plate, reserving grease in the skillet.',
+      'Heat tortillas in reserved grease in the skillet over medium heat until warmed, 1 to 2 minutes per side. Stack 2 tortillas for each taco, then fill with chorizo, onion, and cilantro.']
+    },
+    {
+      name: 'Easy Chorizo Street Tacos',
+      subtitle: 'Time taken: 20min',
+      img: 'https://ionicframework.com/docs/img/demos/card-media.png',
+      description: 'Get street-style tacos without ever leaving your house. Mexican chorizo brings the heat here. These chorizo tacos use just 5 ingredients and taste amazing.',
+      ingredients: ['Chorizo sausage link (1)', 'Chipotle peppers in adobo sauce (2 tbsp)', 'Corn tortillas (2)', 'Chopped onion (2 tbsp) or to tase', 'Chopped fresh cilantro (optional) (2 tbsp)'],
+      nutrition: [],
+      directions: [ 'Combine crumbled chorizo and chipotle peppers in adobo sauce in a bowl.',
+      'Heat a skillet over medium-high heat; add chorizo mixture and cook until crisp, 5 to 7 minutes. Transfer to a plate, reserving grease in the skillet.',
+      'Heat tortillas in reserved grease in the skillet over medium heat until warmed, 1 to 2 minutes per side. Stack 2 tortillas for each taco, then fill with chorizo, onion, and cilantro.']
+    },
+    {
+      name: 'Lemon Garlic Butter Salmon',
+      subtitle: 'Time taken: 30min',
+      img: 'https://ionicframework.com/docs/img/demos/card-media.png',
+      description: 'Juicy salmon at home in 30 minutes that’s easy and tastes better than from a restaurant! The butter is infused with lemon and garlic and adds so much flavor!',
+      ingredients: ['Salmon fillet (1.5 lbs)', 'Lemon (1)', 'Unsalted butter (1/2 cup)', 'Honey (2 tbsp)', 'Garlic, pressed or minced (5 cloves)', 'Kosher salt (1 tsp)', 'Ground black pepper (1/2 tsp)', 'Parsley (2 tsp)'],
+      nutrition: [],
+      directions: [ 'Preheat oven to 375F. Place salmon on a sheet of foil (with the longer side parallel with the longer side of the sheet pan) with the skin-side down. Raise the edges of the foil up about 2 inches (or enough so that when you pour the buttery mixture over the top it will be contained in the foil).',
+      'Nestle lemon slices underneath the salmon, spaced evenly around the fillet; set aside.',
+      'To a microwave-safe glass measuring cup or bowl, add the butter and heat on high power to melt, about 1 minute.',
+      'Stir in the lemon juice, honey, and garlic. Pour or spoon about three-quarters of the mixture over the salmon; reserve remainder.',
+      'Evenly season salmon with salt and pepper.',
+      'Seal up the foil. Likely you will need to add another sheet of foil on top in order to seal it. Crimp or pinch the top and bottom pieces of foil together; get it as air-tight as possible but it does not have to be perfect. If you have time, set aside to marinate for about 10 to 15 minutes (enhanced flavor).',
+      'Place foil pack on a baking sheet and bake for about 17 minutes.',
+      'Remove baking sheet from the oven and remove the top piece of foil or open up the packet so salmon is exposed but keep the edges raised to contain the buttery mixture.',
+      'Spoon the reserved buttery mixture over the salmon, if desired. Use your judgment and if there’s already lots of juice you don’t need to add it because you don’t want it to start leaking. If you have extra buttery mixture, it’s great served over the finished dish.',
+      'Optionally garnish salmon with parsley and serve immediately']
+    },
+    {
+      name: 'Avocado Chicken Bowl',
+      subtitle: 'Time taken: 20min',
+      img: 'https://ionicframework.com/docs/img/demos/card-media.png',
+      description: 'Healthy avocado chicken bowls are easy to meal prep for the week and come with a simple low carb option!',
+      ingredients: ['Boneless, skinless chicken thights (1 lb)', 'Cilantro (1 tsp)', 'Cumin (1 tsp)', 'Crushed rep pepper (1 tsp)', 'Black pepper (t tsp)', 'Black beans (2/3 cup)', 'Olive oil (2 tbsp)', 'Arugula (1 cup)', 'Spanish olives (1/2 cup)', 'Broccoli florets (2 cups)', 'Lettuce (3 cups)'],
+      nutrition: [],
+      directions: [ 'Marinate the chicken with the coriander, cumin, crushed red pepper and black pepper oil for 30 minutes. Or if you don not have time - just rub the chicken with the spices and cook it.',
+      'Cook in a non-stick pan at medium heat with the 1 Tbsp olive oil until done.',
+      'Stir-fry the broccoli in another pan with some olive oil.',
+      'Rinse and drain the black beans, the olives (watch out for salt content!), chop lettuce, arugula and cilantro.',
+      'Arrange the bowls with some greens at the bottom, olives, beans, chicken, broccoli and sauces on top.', 
+      'Serve!']
+    },
+    {
+      name: 'Rise and Shine Parfait',
+      subtitle: 'Time taken: 15min',
+      img: 'https://ionicframework.com/docs/img/demos/card-media.png',
+      description: 'Start your day with a smile. This fruit, yogurt and granola parfait is so easy to make. If you like, use whatever favorite fresh fruits are in season and are looking best at the supermarket',
+      ingredients: ['Vanilla yogurt of choice (4 cups)', 'Medium sized peaches (2)', 'Freh blackberries (2 cups)', 'Granola (1/2 cup)'],
+      nutrition: [],
+      directions: ['Layer half the yogurt, peaches, blackberries and granola into 4 parfait glasses.', 
+      'Repeat layers.']
+    },
+    {
+      name: 'No-bake Cereal Bars',
+      subtitle: 'Time taken: 15min',
+      img: 'https://ionicframework.com/docs/img/demos/card-media.png',
+      description: 'A delicious, nutritious, crunchy, munchy bar to take on the go during those busy mornings! These bars gives you all your breakfast nutrients in a few bites.',
+      ingredients: ['Cereal (1 cup)', 'Maple syrup (4 tbsp)', 'Almond butter (4 tbsp)'],
+      nutrition: [],
+      directions: [ 'Prep the ingredients: Add the cereal into a large mixing bowl. Next, add the maple syrup and almond butter into a microwave-safe bowl and microwave it for 20 seconds, or until the peanut butter has melted.',
+      'Make the bars: Add the syrup and almond butter mixture to the mixing bowl with cereal and mix until combined.',
+      'Set, slice, and serve: Transfer the cereal bar mixture to a lined pan and refrigerate it for 30 minutes or until it has firmed up. Once firm, slice them into bars.']
+    },
+
 
   
   ];
