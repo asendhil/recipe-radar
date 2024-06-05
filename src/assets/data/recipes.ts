d14d07b3414d25a479d84ae2210565f40492b9ab
Sanika Bhamare <sanikabhamare@gmail.com>
2024-06-05 16:17:05 -0700

    Added new fields to array db

--- src/assets/data/recipes.ts
+++ src/assets/data/recipes.ts
@@ -4,18 +4,27 @@ export const recipes = [
       subtitle: 'Time taken: 30min',
       img: 'https://ionicframework.com/docs/img/demos/card-media.png',
       description: 'A classic Italian pasta dish with a rich tomato sauce.',
+      ingredients: [],
+      nutrition: [],
+      directions:[]
     },
     {
       name: 'Grilled Chicken',
       subtitle: 'Time taken: 25min',
       img: 'https://ionicframework.com/docs/img/demos/card-media.png',
       description: 'Juicy grilled chicken with herbs and spices.',
+      ingredients: [],
+      nutrition: [],
+      directions:[]
     },
     {
       name: 'Vegetable Stir Fry',
       subtitle: 'Time taken: 20min',
       img: 'https://ionicframework.com/docs/img/demos/card-media.png',
       description: 'A healthy mix of fresh vegetables.',
+      ingredients: [],
+      nutrition: [],
+      directions:[]
     },
 
   
