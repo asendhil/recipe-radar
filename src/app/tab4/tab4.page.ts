87a3a6e38efd6937052b9f559288d6bcdd093b6b
Sanika Bhamare <sanikabhamare@gmail.com>
2024-06-06 16:55:11 -0700

    dietary restrictions and added other option

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tab4.page.scss'],
 })
 export class Tab4Page implements OnInit {
-  restrictions = ["Dairy", "Peanut", "Soy", "Egg", "Seafood", "Sulfite", "Gluten", "Sesame", "Treenut", "Grain", "Shellfish", "Wheat"];
+  restrictions: string[] = ["Dairy", "Peanut", "Soy", "Egg", "Seafood", "Sulfite", "Gluten", "Sesame", "Treenut", "Grain", "Shellfish", "Wheat"];
   selectedRestrictions: string[] = [];
+  newRestriction: string = '';
 
-  constructor() { }
+  constructor() {
+    this.restrictions.sort((a, b) => a.localeCompare(b));
+  }
 
   ngOnInit() {
   }
@@ -25,5 +28,26 @@ export class Tab4Page implements OnInit {
       }
     }
     console.log(this.selectedRestrictions); // For debugging purposes
+  }
+
+  addOtherRestriction() {
+    const trimmedRestriction = this.newRestriction.trim();
+    if (trimmedRestriction && !this.selectedRestrictions.includes(trimmedRestriction)) {
+      this.selectedRestrictions.push(trimmedRestriction);
+      this.newRestriction = ''; // Clear the input
+    }
+    console.log(this.selectedRestrictions); // For debugging purposes
+  }
+
+  removeRestriction(restriction: string) {
+    const index = this.selectedRestrictions.indexOf(restriction);
+    if (index > -1) {
+      this.selectedRestrictions.splice(index, 1);
+    }
+    console.log(this.selectedRestrictions); // For debugging purposes
+  }
+
+  getNewRestrictions(): string[] {
+    return this.selectedRestrictions.filter(restriction => !this.restrictions.includes(restriction));
   }
 }
