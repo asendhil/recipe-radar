ed225972d6b2aa816c8fb47103ad2ccc5688c529
Sanika Bhamare <sanikabhamare@gmail.com>
2024-06-06 16:13:03 -0700

    successfully designed (finally) dietary intolerances checkboxes

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tab4.page.scss'],
 })
 export class Tab4Page implements OnInit {
+  restrictions = ["Dairy", "Peanut", "Soy", "Egg", "Seafood", "Sulfite", "Gluten", "Sesame", "Treenut", "Grain", "Shellfish", "Wheat"];
+  selectedRestrictions: string[] = [];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onCheckboxChange(event: CustomEvent, restriction: string) {
+    const isChecked = event.detail.checked;
+    if (isChecked) {
+      this.selectedRestrictions.push(restriction);
+    } else {
+      const index = this.selectedRestrictions.indexOf(restriction);
+      if (index > -1) {
+        this.selectedRestrictions.splice(index, 1);
+      }
+    }
+    console.log(this.selectedRestrictions); // For debugging purposes
+  }
 }
